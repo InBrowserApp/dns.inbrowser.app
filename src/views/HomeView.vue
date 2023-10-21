199392731c9b5c49737b358572399cf137288c2d
Seedgou <i@zczc.cz>
2023-10-21 16:25:02 +0800

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <n-h2 prefix="bar" align-text>Tools List</n-h2>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH2 } from "naive-ui";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "DNS InBrowser.App",
+  meta: [
+    {
+      name: "description",
+      content:
+        "DNS InBrowser.App is a tool to check DNS. Fully runs in your browser. No server-side code.",
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: "https://dns.inbrowser.app/",
+    },
+  ],
+});
+</script>
