50bde1f69b4fe224e108cb4b04b0ff454295fa92
Seedgou <i@zczc.cz>
2023-10-21 16:35:11 +0800

    add dns lookup view

---
+++ src/views/DNSLookupView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <n-h2 prefix="bar" align-text>DNS Lookup</n-h2>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH2 } from "naive-ui";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "DNS Lookup | DNS InBrowser.App",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Lookup DNS records. Fully runs in your browser. No server-side code.",
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: "https://dns.inbrowser.app/tools/dns-lookup",
+    },
+  ],
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
   <main>
     <n-h2 prefix="bar" align-text>Tools List</n-h2>
+    <router-link to="/tools/dns-lookup" #="{ navigate, href }" custom>
+      <n-button tag="a" :href="href" @click="navigate" text>
+        <template #icon>
+          <n-icon :component="DocumentSearch16Regular" />
+        </template>
+        DNS Lookup
+      </n-button>
+    </router-link>
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH2 } from "naive-ui";
+import { NH2, NButton, NIcon } from "naive-ui";
+import { DocumentSearch16Regular } from "@vicons/fluent";
 import { useHead } from "@vueuse/head";
 
 useHead({
