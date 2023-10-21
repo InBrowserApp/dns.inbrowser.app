4ce9157413bbd16a95d54d690ec4c2e44656d946
Seedgou <i@zczc.cz>
2023-10-21 18:41:57 +0800

    use doh dns-json

--- src/components/tools/dns-lookup/DNSLookup.vue
+++ src/components/tools/dns-lookup/DNSLookup.vue
@@ -30,17 +30,18 @@ import { ref } from "vue";
 import DomainInput from "./DomainInput.vue";
 import DOHServerSelect from "@/components/doh/DOHServerSelect.vue";
 import { NButton, NCollapse, NCollapseItem, NIcon, NH3 } from "naive-ui";
-import doh from "@/utils/packages/dohjs";
 import { Settings16Regular } from "@vicons/fluent";
 import DNSRawResult from "./DNSRawResult.vue";
+import { makeDOHQuery } from "@/utils/dns/doh";
 
 const domain = ref("example.com");
 const dohServer = ref("https://cloudflare-dns.com/dns-query");
 const result = ref("");
 
 async function lookup() {
-  const resolver = new doh.DohResolver(dohServer.value);
-  const response = await resolver.query(domain.value, "A", "GET");
+  const response = await makeDOHQuery(dohServer.value, {
+    name: domain.value,
+  });
   result.value = JSON.stringify(response, null, 2);
 }
 </script>
--- src/utils/dns/doh/index.ts
+++ src/utils/dns/doh/index.ts
@@ -1,1 +1,2 @@
 export { BUILTIN_DOH_SERVERS } from "./servers";
+export { makeDOHQuery } from "./query";
