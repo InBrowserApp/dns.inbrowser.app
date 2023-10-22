eb3c804472708d71817e3c30f577f17def3a94cd
Seedgou <i@zczc.cz>
2023-10-22 20:24:12 +0800

    update icon style

--- src/components/tools/dns-lookup/query/DNSQuery.vue
+++ src/components/tools/dns-lookup/query/DNSQuery.vue
@@ -2,29 +2,35 @@
   <n-h3 prefix="bar" align-text>Query</n-h3>
 
   <DomainInput v-model:domain="domain" />
-  <RecordTypeInput v-model:type="recordType" />
 
-  <n-collapse>
-    <template #arrow>
+  <!-- in same row -->
+  <n-grid cols="2" x-gap="15">
+    <n-grid-item>
+      <RecordTypeInput v-model:type="recordType" />
+    </n-grid-item>
+    <n-grid-item>
+      <DOHServerSelect v-model:value="dohServer" />
+    </n-grid-item>
+  </n-grid>
+
+  <n-button @click="lookup" round>
+    <template #icon>
       <n-icon>
-        <Settings16Regular />
+        <DocumentSearch16Regular />
       </n-icon>
     </template>
-    <n-collapse-item title="Advanced Settings" name="advanced-settings">
-      <DOHServerSelect v-model:value="dohServer" />
-    </n-collapse-item>
-  </n-collapse>
-  <n-button @click="lookup" type="primary"> Lookup </n-button>
+    Lookup
+  </n-button>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 import DomainInput from "./DomainInput.vue";
 import DOHServerSelect from "@/components/doh/DOHServerSelect.vue";
-import { NButton, NCollapse, NCollapseItem, NIcon, NH3 } from "naive-ui";
-import { Settings16Regular } from "@vicons/fluent";
+import { NButton, NH3, NGrid, NGridItem, NIcon } from "naive-ui";
 import { makeDOHQuery } from "@/utils/dns/doh";
 import RecordTypeInput from "./RecordTypeInput.vue";
+import { DocumentSearch16Regular } from "@vicons/fluent";
 
 const recordType = ref("A");
 const domain = ref("example.com");
