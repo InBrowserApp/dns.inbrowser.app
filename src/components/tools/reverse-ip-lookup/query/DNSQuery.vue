c5e2d7ebd6f5edd5d86ee73d58b31ebbb3ca4b04
Seedgou <i@zczc.cz>
2023-10-23 00:02:04 +0800

    move reverse ip domian to separate file

--- src/components/tools/reverse-ip-lookup/query/DNSQuery.vue
+++ src/components/tools/reverse-ip-lookup/query/DNSQuery.vue
@@ -21,6 +21,7 @@ import DOHServerSelect from "@/components/doh/DOHServerSelect.vue";
 import { NButton, NH3, NIcon } from "naive-ui";
 import { makeDOHQuery } from "@/utils/dns/doh";
 import { DocumentSearch16Regular } from "@vicons/fluent";
+import { ReverseIPDomain } from "@/utils/dns/common/reverse-ip-domain";
 
 const ip = ref("2606:4700:4700::1111");
 const dohServer = ref("https://cloudflare-dns.com/dns-query");
@@ -31,14 +32,7 @@ const emit = defineEmits(["update:result"]);
 async function lookup() {
   loading.value = true;
   try {
-    let reverseIPDomain = "";
-    if (ip.value.includes(":")) {
-      reverseIPDomain = toIPv6Arpa(ip.value);
-    } else {
-      reverseIPDomain =
-        ip.value.split(".").reverse().join(".") + ".in-addr.arpa";
-    }
-
+    const reverseIPDomain = ReverseIPDomain(ip.value);
     const response = await makeDOHQuery(dohServer.value, {
       name: reverseIPDomain,
       type: "PTR",
@@ -49,42 +43,5 @@ async function lookup() {
   } finally {
     loading.value = false;
   }
-}
-
-function toIPv6Arpa(ipv6) {
-  // Step 1: Expand the IPv6 address to its full form
-  let expandedIPv6 = expandIPv6Address(ipv6);
-  // Step 2: Remove the colons
-  let noColons = expandedIPv6.replace(/:/g, "");
-  // Step 3: Reverse the order of the characters
-  let reversed = noColons.split("").reverse().join("");
-  // Step 4: Insert dots between every character
-  let dotted = reversed.split("").join(".");
-  // Step 5: Append .ip6.arpa to the end
-  let arpaFormat = dotted + ".ip6.arpa";
-
-  return arpaFormat;
-}
-
-function expandIPv6Address(address) {
-  let fullAddress = "";
-  let segments = address.split("::");
-  if (segments.length === 2) {
-    let segment1 = segments[0].split(":");
-    let segment2 = segments[1].split(":");
-    let missingZeroes = 8 - (segment1.length + segment2.length);
-    fullAddress = segment1.join(":");
-    for (let i = 0; i < missingZeroes; i++) {
-      fullAddress += ":0000";
-    }
-    fullAddress += ":" + segment2.join(":");
-  } else {
-    fullAddress = address;
-  }
-  let hextets = fullAddress.split(":");
-  for (let i = 0; i < hextets.length; i++) {
-    hextets[i] = ("0000" + hextets[i]).slice(-4);
-  }
-  return hextets.join(":");
 }
 </script>
